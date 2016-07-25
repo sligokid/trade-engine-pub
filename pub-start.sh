5e306d261e90b5e59926bf1b3e709b1565b36180
Ubuntu <ubuntu@ip-172-31-9-107.eu-west-1.compute.internal>
2016-07-25 21:24:55 +0000

    changing startup script

--- pub-start.sh
+++ pub-start.sh
@@ -1,1 +1,3 @@
-java -jar target/currency-fair-pub-0.0.1-SNAPSHOT.jar &> log.term 
+DATE=`date "+%Y-%m-%d"`
+mvn spring-boot:run &> log.${DATE}.term
+#java -jar target/currency-fair-api-0.0.1-SNAPSHOT.jar &> log.term
